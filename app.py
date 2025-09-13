b2739ff0b6e3b79da13795c4ed63c65a3abef39b
WalidLab <walid.labidi001@gmail.com>
2025-09-13 14:24:31 +0100

    modif app.py

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
+import uvicorn
 from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/hello")
-def hello():
+@app.get("/")
+def read_root():
     return {"message": "Hello from FastAPI on OpenShift!"}
+
+if __name__ == "__main__":
+    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=False)
